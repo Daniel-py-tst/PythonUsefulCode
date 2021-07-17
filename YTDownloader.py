22ef008476b968b579e053e91170894f82032b51
Daniel-py-tst <82398893+Daniel-py-tst@users.noreply.github.com>
2021-07-16 20:58:08 -0400

    Create YTDownloader.py

---
+++ YTDownloader.py
@@ -0,0 +1,22 @@
+#  imports
+from pytube import YouTube
+
+# url
+url = 'https://www.youtube.com/watch?v=tb8gHvYlCFs'
+
+# get the video from the url
+video = YouTube(url)
+
+# get the title
+title = video.title
+
+# print(title)
+
+# downloading the video
+stream = video.streams.first()
+
+# downloading to cwd
+stream.download()
+
+print('Download Complete')
+ #credits codewithbenny
